c6e6eecf38779317ab0e0f59135a82db7306dc96
Buddhi Prabodhani Kandambi <80638903+lkprabha@users.noreply.github.com>
2024-04-03 22:41:11 +1100

    First Commit

---
+++ src/main/java/com/bookstore/controller/BookContoller.java
@@ -0,0 +1,71 @@
+package com.bookstore.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookstore.entity.Book;
+import com.bookstore.entity.MyBookList;
+import com.bookstore.service.BookService;
+import com.bookstore.service.MyBookListService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class BookContoller {
+	@Autowired
+	private BookService service;
+	
+	@Autowired
+	private MyBookListService myBookService;
+	
+	@GetMapping("/")
+	public String Home() {
+		return "home";
+	}
+	@GetMapping("/book_register")
+	public String bookRegister() {
+		return "bookRegister";
+	} 
+	@GetMapping("/available_books")
+	public ModelAndView getAllBooks() {
+		List<Book> list=service.getAllBooks();
+//		ModelAndView m=new ModelAndView();
+//		m.setViewName("bookList");
+//		m.addObject("book",list);
+		
+		return new ModelAndView("bookList","book",list);
+	} 
+	@PostMapping("/save")
+	public String addBook(@ModelAttribute Book b) {
+		service.save(b);
+		return "redirect:/available_books";
+	}
+	@GetMapping("/my_books")
+	public String getMyBooks(Model model) {
+		List<MyBookList> list = myBookService.getAllMyBooks();
+		model.addAttribute("book",list);
+		return "myBooks";
+	}
+	
+	@GetMapping("/mylist/{id}")
+	public String getMyList(@PathVariable("id")int id) {
+		Book b= service.getBookById(id);
+		MyBookList mb=new MyBookList(b.getId(),b.getName(),b.getAuthor(),b.getPrice());
+		myBookService.SaveMyBooks(mb);
+		return "redirect:/my_books";
+	}
+	
+	
+	}
+	
+	
+
